544e61dc44c1590fb8f27e83ff1829b01ce54e17
gabrielopes7 <gabrielplopes7@hotmail.com>
2024-07-14 21:25:58 -0300

    Feat: Densenvolvimento da página principal do site, uma página de login e seus estilos

---
+++ src/pages/Login - Cópia/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import LoginPresentation from './components/LoginPresentation';
+
+
+export default function Login(){
+    return <>
+        <LoginPresentation/>
+    </>
+}
